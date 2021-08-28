69a1fbd677b25b751988cdd5a8790e4ce135e7a2
Troy Flavell <hunter007nz@gmail.com>
2021-08-29 01:12:36 +1200

    Added Theme and start of grid

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+
+const theme1 = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: '#fff',
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+  return (<ThemeProvider theme={theme1} > <Component {...pageProps} /> </ThemeProvider>)
 }
+
+
 
 export default MyApp
